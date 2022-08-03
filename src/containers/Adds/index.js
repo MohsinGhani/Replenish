ebd262e5d32adae9f9ce13189ebc0a59c1e7a452
unknown <Mohsin Ghani>
2022-08-03 20:47:49 +0500

    fix build issue

--- src/containers/Adds/index.js
+++ src/containers/Adds/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import Ads from "src/components/adsPage/ads";
 
 const AdsPage = () => {
-	return (
-		<>
-			<Ads/>
-		</>
-
-	);
+	return <Ads />;
 };
 
 export default AdsPage;
