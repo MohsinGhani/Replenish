81135f8a1eb71c9541ce801c900e33e51a77e3d9
unknown <Mohsin Ghani>
2022-08-07 14:05:00 +0500

    sidebar fix

--- src/components/homepage/landingBanner/index.js
+++ src/components/homepage/landingBanner/index.js
@@ -3,7 +3,7 @@ import { Image, Grid } from "antd";
 import BookNowDropown from "src/common/bookNowDropdown/index.js";
 import { BannerLogo } from "src/components/SVGImageIcon/bannerLogo";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFade, Navigation, Pagination, Autoplay } from "swiper";
+import { EffectFade, Navigation, Pagination, Autoplay, Lazy } from "swiper";
 import { BannerImgs } from "./bannerData";
 
 const { useBreakpoint } = Grid;
@@ -39,7 +39,7 @@ const LandingBanner = () => {
 				</div>
 			</div>
 			<Swiper
-				modules={[EffectFade, Navigation, Pagination, Autoplay]}
+				modules={[EffectFade, Navigation, Pagination, Autoplay, Lazy]}
 				speed={5000}
 				effect="fade"
 				// navigation
@@ -48,6 +48,7 @@ const LandingBanner = () => {
 				// onSwiper={(swiper) => console.log(swiper)}
 				// onSlideChange={() => console.log("slide change")}
 				autoplay
+				lazy
 				className="banners"
 			>
 				{BannerImgs?.map((banner, index) => (
